8426fe62fbd2138fe479c7deed99be50a380cb06
elizagb <egerblack@gmail.com>
2024-04-04 16:38:54 -0700

    double jump feature added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,6 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float speed = 0;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
@@ -15,8 +14,10 @@ public class PlayerController : MonoBehaviour
     private int count;
     private float movementX;
     private float movementY;
+    private float jumpForce;
+    private int remainingJumps;
 
-    // Start is called before the first frame update
+    // Start called before 1st frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -24,6 +25,8 @@ public class PlayerController : MonoBehaviour
         SetCountText();
         winTextObject.SetActive(false);
 
+        jumpForce = 5f;
+        remainingJumps = 2; // Allow double jump
     }
 
     void OnMove(InputValue movementValue)
@@ -43,25 +46,39 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        // If jumps remaining and space key pressed
+        if (remainingJumps > 0 && Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            remainingJumps--;
+        }
+
+    }
+
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
-
     }
 
     void OnTriggerEnter(Collider other)
     {
-
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
             count = count + 1;
             SetCountText();
         }
-        
-
     }
 
-
+    void OnCollisionEnter(Collision collision)
+    {
+        // If player collides with the object tagged as Ground
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            remainingJumps = 2; // Reset remaining jumps
+        }
+    }
 }
